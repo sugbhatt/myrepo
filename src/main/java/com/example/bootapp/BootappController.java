ca91af55c311da32fab4c7d4d780e09058b5fba9
Bhattacherjee <683211@cognizant.com>
2018-07-09 15:23:35 +0530

    add all files

---
+++ src/main/java/com/example/bootapp/BootappController.java
@@ -0,0 +1,18 @@
+package com.example.bootapp;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BootappController {
+
+    @RequestMapping("/")
+    public String greeting() throws UnknownHostException{
+        return "Hello Boot! Date: " + new SimpleDateFormat("yyyy-MM-dd:HH.mm.ss").format(new Date()) + " from Host: " + InetAddress.getLocalHost().getHostName(); 
+    }
+}
